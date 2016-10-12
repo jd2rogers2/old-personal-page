4b7d624505d001caf0db742ecc243009f25f55d2
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-12 19:24:32 -0400

    played around with js but to no avail, didn't learn much bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
   hideSubPosts();
+  clickMoreLess();
 });
 
-function togglePosts(a_tag) {
-  debugger;
-  a_tag.parentElement.parentElement.find('.subPost').slideToggle();
+function clickMoreLess(){
+  $('.moreLess').on("click", function(event){
+    debugger;
+    $('.moreLess').parentElement.children.forEach(function(element){
+      element.toggleClass('.subPost');
+    });
+    // togglePost(this);
+    event.preventDefault();
+  });
+}
+
+function togglePost(a_tag) {
+  // debugger;
+  
+  // a_tag.parentElement.parentElement.find('.subPost').slideToggle();
+  // event.preventDefault();
 }
 
 function hideSubPosts(){
